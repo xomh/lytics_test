132ed240eb6a49d50ed79e3a49f98a79c08cc560
Xochitl Martinez Hernandez <mhxochitl@gmail.com>
2021-06-03 01:22:16 -0500

    READ.md completed and the script modified to obtain the objectives with functions.

--- README.md
+++ README.md
@@ -1,1 +1,49 @@
 # lytics_test
+This Python Script has as objective:
+
+
+* Obtain from variable ``url`` automatically COVID-19 cases in USA.
+    * In function ``create()``:
+        ```bash
+        url = PATH #url is a parameter (csv file url to its raw url format) passed to the read_csv() function to obtain the data record
+        ```
+
+* Set the info in a Pandas Dataframe.
+    * In function ``create()``:
+        ```bash
+        df = pd.read_csv(PATH) #df is our Dataframe
+        ```
+
+* Show a table with total cases per day.
+    * In function ``showtable()``:
+        ```bash
+        df = casesperday() #create our Dataframe with a new column with the cases per day using the 'cases' column (daily cumulative number of cases)
+
+        df_res = df[['columnns']].copy() #create a copy of Dataframe only with the necessary columns
+
+        #finally print the table with total cases per day
+        ```
+* Display the chart with the top ten days with the most cases.
+    * In function ``displaychart(bool)``:
+        ```bash
+        df.sort_values() #sort Dataframe to obtain the top ten
+        
+        column = df_aux.head(10)['column'].tolist() #convert a column with the first ten values to a list
+        
+        column.reverse() #for a better visualization of the chart, the values are ordered in reverse order
+        
+        axis = np.array(column) #set the values in x and y axis
+
+        #set the properties to the chart view
+        
+        if bool: #if the bool value is true, the window where the chart is going to be seen is resize to the max size
+        ```
+* Save the chart in local storage.
+     * In function ``displaychart(bool)``:
+        ```bash
+        #if the bool value is false, create the "Get the current figure" with the function gcf() and after save it:
+        
+        else:
+            fig = plt.gcf()
+            fig.savefig("name.png")
+        ```
--- lytics_test.py
+++ lytics_test.py
@@ -3,39 +3,49 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 
-#Obtener desde url el registro de casos por Covid en Estados Unidos.
-url = 'https://raw.githubusercontent.com/nytimes/covid-19-data/master/us.csv'
+def create():
+    url = 'https://raw.githubusercontent.com/nytimes/covid-19-data/master/us.csv'
+    df = pd.read_csv(url)
+    return df
 
-#Guardar los datos en un Dataframe de Pandas.
-df = pd.read_csv(url)
+def casesperday():
+    df = create()
+    auxlist = df['cases'].to_list()
+    daycases = []
+    for i in range(len(auxlist)):
+        if i == 0:
+            daycases.append(auxlist[i])
+        else:
+            daycases.append(auxlist[i] - auxlist[i-1])
+    df['daycases'] = daycases
+    return df
 
-#Generar una tabla con el resumen total de casos por día.
-df_res = df[['date', 'cases']].copy()
-print(df_res.to_string(index=False))
+def showtable():
+    df = casesperday()
+    df_res = df[['date', 'daycases']].copy()
+    print(df_res.to_string(index=False))
 
-#Generar gráfica con el top 10 de días con más casos.
-auxlist = df['cases'].to_list()
-daycases = []
-for i in range(len(auxlist)):
-    if i == 0:
-        daycases.append(auxlist[i])
+def displaychart(show):
+    df = casesperday()
+    df_aux = df.sort_values('daycases', ascending=False)
+    datelist = df_aux.head(10)['date'].tolist()
+    caseslist = df_aux.head(10)['daycases'].tolist()
+    datelist.reverse()
+    caseslist.reverse()
+    x = np.array(datelist)
+    y = np.array(caseslist)
+    plt.barh(x, y, height = 0.5, color = "magenta")
+    plt.grid(color = 'gray', linestyle = '--', linewidth = 0.5, axis = 'x')
+    plt.title("Top ten of days with the most COVID-19 cases in USA", loc = 'right')
+    if show:
+        manager = plt.get_current_fig_manager()
+        manager.resize(*manager.window.maxsize())
+        plt.show()
     else:
-        daycases.append(auxlist[i] - auxlist[i-1])
-df['daycases'] = daycases
-df_aux = df.sort_values('daycases', ascending=False)
-datelist = df_aux.head(10)['date'].tolist()
-caseslist = df_aux.head(10)['daycases'].tolist()
-datelist.reverse()
-caseslist.reverse()
-x = np.array(datelist)
-y = np.array(caseslist)
-plt.barh(x, y, height = 0.5, color = "magenta")
-plt.grid(color = 'gray', linestyle = '--', linewidth = 0.5, axis = 'x')
-plt.title("Top 10 de días con más casos de COVID en Estados Unidos.", loc = 'right')
-manager = plt.get_current_fig_manager()
-manager.resize(*manager.window.maxsize())
-fig = plt.gcf()
-plt.show()
+        fig = plt.gcf()
+        fig.savefig("top10.png")
 
-#Guardar la gráfica en un archivo local.
-fig.savefig("top10.png")
+
+showtable()
+displaychart(True)
+displaychart(False)
